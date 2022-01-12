a01e084085abcead3e14c5059d041575e53abad0
SeedPuller <SeedPuller@gmail.com>
2022-01-13 02:01:13 +0330

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-97b7418dc930dd64459b.js"
+    "url": "webpack-runtime-7ede1d9a2e006a80cc6e.js"
   },
   {
     "url": "framework-0c07c9218e9798907e47.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "08d76e5e4fe60a3a660849482161e648"
+    "revision": "a1d27076087b28183267a571c57c65ef"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-77466cc0b3ed4dac08eb.js"
